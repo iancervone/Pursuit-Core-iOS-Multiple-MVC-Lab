506592f7ff5f78505f15a344479e5dba67bc59a1
Ian <ian@Ians-MacBook-Pro.local>
2019-08-29 14:51:10 -0400

    got some animals

---
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/AnimalListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnimalListTableViewCell.swift
+//  AC-iOS-Multiple-MVC
+//
+//  Created by Ian Cervone on 8/13/19.
+//  Copyright © 2019 AC-iOS. All rights reserved.
+//
+
+import UIKit
+
+class AnimalListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
